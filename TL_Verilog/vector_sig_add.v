efd6052d297a69c207032421f5033b05691a7a41
vishal-hunashikatti <vishalb.hunashikatti@gmail.com>
2024-05-27 17:34:43 +0200

    Added Initial Simulation files for my reference

---
+++ TL_Verilog/vector_sig_add.v
@@ -0,0 +1,22 @@
+\m5_TLV_version 1d: tl-x.org
+\m5
+   
+   // =================================================
+   // Welcome!  New to Makerchip? Try the "Learn" menu.
+   // =================================================
+   
+   //use(m5-1.0)   /// uncomment to use M5 macro library.
+\SV
+   // Macro providing required top-level module definition, random
+   // stimulus support, and Verilator config.
+   m5_makerchip_module   // (Expanded in Nav-TLV pane.)
+\TLV
+   $reset = *reset;
+   
+   $out[7:0] = $in1[6:0] + $in2[6:0];
+   
+   // Assert these to end simulation (before Makerchip cycle limit).
+   *passed = *cyc_cnt > 40;
+   *failed = 1'b0;
+\SV
+   endmodule
